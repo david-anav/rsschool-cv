c1949acf0d37532b0e5d19ace80c9f796aadcd6d
david-anav <art_migalev@yahoo.com>
2024-03-22 12:31:06 +0300

    modified:   index.html 	modified:   index.js 	deleted:    sass/layout/_footer.scss 	modified:   sass/layout/_main.scss 	new file:   sass/layout/modal.css 	new file:   sass/layout/modal.css.map 	new file:   sass/layout/modal.scss 	modified:   sass/style.css 	modified:   sass/style.css.map 	modified:   sass/style.scss

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const certif = document.querySelector(".python-certificate");
+
 window.onload = () => {
   certif.addEventListener("click", (evnt) => {
-
     if (evnt.target) {
-
+      createdModal(certif);
     }
+    const modal = document.querySelector('.modal-bg')
+    modal.addEventListener('click', (ev)=>{
+      if(modal.contains(ev.target)){
+        modal.classList.remove('modal-bg')
+      }
+    })
   });
 };
 
-const createdModal = () => {
-  
-}
+const createdModal = (child) => {
+  const fragmentModal = new DocumentFragment();
+  const bgModal = document.createElement("div");
+  bgModal.className = "modal-bg";
+  const modal = document.createElement("dialog");
+  modal.className = "md-dialog";
+  modal.appendChild(child);
+  bgModal.appendChild(modal);
+  fragmentModal.appendChild(bgModal);
+  document.body.appendChild(fragmentModal);
+};
